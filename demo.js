78149bcda9c4902a568be61125cec020d9836e7b
nirnay01 <ravalnirnay@gmail.com>
2024-01-24 11:53:42 +0530

    try

---
+++ demo.js
@@ -0,0 +1,18 @@
+import { Gmail } from '@google-cloud/gmail';
+
+const gmail = new Gmail({
+  projectId: 'send-gmail-412206',
+  keyFilename: 'API key 1',
+});
+
+const message = {
+  from: 'nirnayraval20@gnu.ac.in',
+  to: 'ravalnirnay@gmail.com',
+  subject: 'Hello from Gmail API!',
+  text: 'Hi! This is a test message from Gmail API!',
+};
+
+await gmail.users.messages.send({
+  userId: 'nirnayraval20@gnu.ac.in',
+  resource: message,
+});
