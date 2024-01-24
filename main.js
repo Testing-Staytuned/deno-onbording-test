0732980dbc803c401cda373f360d61169fc7134e
nirnay01 <ravalnirnay@gmail.com>
2024-01-24 10:39:19 +0530

    try

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const port = 3000;
 
 // Serve static files (like index.html)
 app.use(serveStatic(Deno.cwd()));
-console.log("hi");
+console.log("hi", Deno.cwd());
 // Serve a specific page on a specific URL
 // app.use("/Email-verify.html", async (req, res) => {
 //   await sendFile(res, `${Deno.cwd()}/Email-verify.html`);
