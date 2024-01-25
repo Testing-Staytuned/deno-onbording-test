075ebafc1181517c9e67a5fec425b3d29c86c516
nirnay01 <ravalnirnay@gmail.com>
2024-01-25 10:49:05 +0530

    try

--- main.js
+++ main.js
@@ -14,7 +14,6 @@ app.use(serveStatic(Deno.cwd()));
 // Handle POST requests for sending verification
 app.post("/send-verification", async (req, res) => {
   const { email, code } = req.query;
-  console.log(email, code);
   try {
     const result = await mailer(email, code);
     res.json({ email, code });
